58e80af0098efe1a0aa1b931cba8cb705f5df69b
Paddy Harker <Paddy@gmail.com>
2024-06-15 09:58:47 +0000

    testing

--- project_incubator/app/__init__.py
+++ project_incubator/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 
 def create_app(config_name):
+    load_dotenv()
     app = Flask(__name__, instance_relative_config=True)
     if config_name == 'testing':
         app.config.from_object('config.TestingConfig')
--- project_incubator/config.py
+++ project_incubator/config.py
@@ -1,6 +1,6 @@
 class Config:
-    SECRET_KEY = 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
 
 class TestingConfig(Config):
     TESTING = True
